47f847929a1e3e91c1e0faa8ba6f7da49fd1b059
Chernyshov <opa.opa359@mail.ru>
2023-02-07 08:46:21 +1000

    ChernushovDA 0IB-1

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -2,13 +2,13 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
 
 @RestController
 public class MainController {
 
     @GetMapping("/")
-    public String hello() {
-        return "Hello, Spring!";
+    public String hello() {Date date = new Date(); return "Чернышов Дмитрий Андреевич " + date.toString();
     }
 
 }
